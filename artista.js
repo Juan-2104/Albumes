b51115ed401fa78d6531c59be6e965867fe20d8d
Juan-2104 <juangonzaleznegocios@gmail.com>
2021-02-27 01:28:19 -0500

    Con toda

---
+++ artista.js
@@ -0,0 +1,27 @@
+function album1(){
+    alert("Album: Animals");
+}
+function album2(){
+    alert("Album: In the name of love");
+}
+function album3(){
+    alert("Album: Scared to be lonely");
+}
+function album4(){
+    alert("Album: There for you");
+}
+function album5(){
+    alert("Album: High on life");
+}
+function album6(){
+    alert("Album: So far away");
+}
+function album7(){
+    alert("Album: Burn out");
+}
+function album8(){
+    alert("Album: Poison");
+}
+function album9(){
+    alert("Album: Pressure");
+}
